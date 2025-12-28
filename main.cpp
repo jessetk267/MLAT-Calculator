0c0a3abfbfe5f5a14c68ea805524788f5f0c24d5
jessetk2-hash <jessetk267@gmail.com>
2025-12-28 10:08:39 -1000

    Seperated MLAT calculator into 3 methods. findRanges(), findPoint(), findError()

--- main.cpp
+++ main.cpp
@@ -10,18 +10,38 @@
  */
 
 int main() {
-
     using Eigen::Vector3d;
-    using Eigen::MatrixXd;
-    using Eigen::VectorXd;
 
-    //Change anchor positions in future to be set by objects
+    Vector3d x_true(0.5, 0.5, 0.5);
+
     std::vector<Vector3d> references = {
         {0.0, 0.0, 0.0},
         {1.0, 0.0, 0.0},
         {0.0, 1.0, 0.0},
         {0.0, 0.0, 1.0}
     };
+
+    std::vector<double> ranges = findRanges(x_true, references);
+
+    Vector3d x_est = findPoint(references, ranges);
+
+    double error = findError(x_true, x_est);
+
+    std::cout << "True position:\n" << x_true.transpose() << "\n\n";
+    std::cout << "Estimated position:\n" << x_est.transpose() << "\n\n";
+    std::cout << "Position error:\n" << error << std::endl;
+
+    return 0;
+}
+
+struct Point3d {
+    double x, y, z;
+};
+
+std::vector<double> findRanges(const Eigen::Vector3d x_true, const std::vector<Eigen::Vector3d> references) {
+    using Eigen::Vector3d;
+    using Eigen::MatrixXd;
+    using Eigen::VectorXd;
 
     const int n = references.size();
 
@@ -33,16 +53,26 @@ int main() {
         S[i] = (x_true - references[i]).norm();
     }
 
+    return S;
+}
+
+Eigen::Vector3d findPoint(const std::vector<Eigen::Vector3d> references, const std::vector<double> ranges) {
+    using Eigen::Vector3d;
+    using Eigen::MatrixXd;
+    using Eigen::VectorXd;
+
+    const int n = references.size();
+
     MatrixXd A(n - 1, 3);
     VectorXd b(n - 1);
 
     const Vector3d& p1 = references[0];
-    const double S1 = S[0];
+    const double S1 = ranges[0];
 
     for (int i = 1; i < n; ++i)
     {
         const Vector3d& pi = references[i];
-        const double Si = S[i];
+        const double Si = ranges[i];
 
         A.row(i - 1) = 2.0 * (p1 - pi).transpose();
 
@@ -52,16 +82,11 @@ int main() {
             - pi.squaredNorm();
     }
 
-    Vector3d x_est =
-        A.jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV)
-         .solve(b);
+    Vector3d x_est = A.jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(b);
 
+    return x_est;
+}
 
-    double error = (x_est - x_true).norm();
-
-    std::cout << "True position:\n" << x_true.transpose() << "\n\n";
-    std::cout << "Estimated position:\n" << x_est.transpose() << "\n\n";
-    std::cout << "Position error:\n" << error << std::endl;
-
-    return 0;
+double findError(const Eigen::Vector3d x_true, const Eigen::Vector3d x_est) {
+    return (x_est - x_true).norm();
 }
